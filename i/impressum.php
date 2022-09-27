37752435861514a2cc4a6736c92d866039b7d07b
Benedikt Galbavy <if22b082@technikum-wien.at>
2022-09-27 11:16:21 +0200

    Moved impressum to seperate file

---
+++ i/impressum.php
@@ -0,0 +1,28 @@
+<div id="Impressum">
+  <h3>Impressum</h3>
+    <p>Hotel xy</p>
+    <p>Inhaber </p>
+    <div id = "Standort">
+      <h4>Standort</h4>
+      <p>Hotel xy</p>
+      <p>Landstraße 43</p>
+      <p>1030 Wien</p>
+      <p>view on map</p>
+    </div>
+    <div id = "Kontakt">
+      <h4>Kontaktdaten</h4>
+      <p>Tel: +43676492233288</p>
+      <p>Mail: office@xy.at</p>
+    </div>
+    <div id = "weitere Informationen">
+      <p>UID-Nr: ATU12345678</p>
+      <p>Das Hotel xy ist Mitglied der WKÖ und WKNÖ</p>
+      <p>Rechtsvorschriften: Gewerbeordnung: <a href="www.ris.bka.gv.at">RIS</a></p>
+      <p>Gewerbebehörde: Bezirkshauptmannschaft Wien</p>
+      <p>Hotel xy ist ein Meisterbetrieb, Meisterprüfung abgelegt in Österreich</p>
+    </div>
+    <div id = "Beschwerde">
+      <p>Verbraucher haben die Möglichkeit,<br>Beschwerden an die <a href="http://ec.europa.eu/odr">Online-Streitbeilegungsplattform der EU</a> zu richten:<br>Sie können allfällige Beschwerde auch an die oben angegebene E-Mail-Adresse richten.
+      </p>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>placeholder</title>
+    <?php include 'head.php'?>
+  </head>
+  <body>
+    <?php include 'navbar.php'?>
+    <main>WIP placeholder for the semester project</main>
+    <?php include 'i/impressum.php'?>
+    <?php include 'footer.php'?>
+  </body>
+</html>
