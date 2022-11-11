3aae29da097125399343aec0e0085a95751d05c4
ossma <oss19129@spengergasse.at>
2022-11-11 17:04:53 +0100

    FileLoader
    you can upload images now

---
+++ i/fileLoader/fileLoader.php
@@ -0,0 +1,7 @@
+<div class="post main" id="FileLoader">
+  <form action="i/fileLoader/upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit" class="btn">
+  </form>
+</div>
