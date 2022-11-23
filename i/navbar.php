46c4c6d63dab6a35d95b092554d87524284ad1e5
Benedikt Galbavy <if22b082@technikum-wien.at>
2022-11-23 10:37:20 +0100

    Code formatting

--- i/navbar.php
+++ i/navbar.php
@@ -3,7 +3,8 @@
 <ul class="navBox" id="navbar" aria-label="Navigation bar">
 	<li class="navPoint">
 		<a class="navLink" href="/index.php">
-			<i class="fas fa-home"></i><p class="navText">&nbsp;Home</p>
+			<i class="fas fa-home"></i>
+			<p class="navText">&nbsp;Home</p>
 		</a>
 	</li>
  	<li class="navPoint">
@@ -26,8 +27,22 @@
 		$url = $_SERVER['REQUEST_URI'];
 ?>
 
-	<li class="navPoint right"><button class="navLink" onclick="openForm('login')"><i class="navIcon fas fa-sign-in-alt"></i><p class="navText">&nbsp;Login</p></button></li>
-<!--<li class="navPoint right"><form method="post" action="signup?signup"><input type="hidden" name="url" value="<?=$url?>" /><button class="navLink" type="submit"><i class="navIcon fas fa-edit"></i><p class="navText">&nbsp;Sign up</p></button></form></ul></li>-->
+	<li class="navPoint right">
+		<button class="navLink" onclick="openForm('login')">
+			<i class="navIcon fas fa-sign-in-alt"></i>
+			<p class="navText">&nbsp;Login</p>
+		</button>
+	</li>
+
+<!--<li class="navPoint right">
+		<form method="post" action="signup?signup"><input type="hidden" name="url" value="<?=$url?>" />
+			<button class="navLink" type="submit">
+				<i class="navIcon fas fa-edit"></i>
+				<p class="navText">&nbsp;Sign up</p>
+			</button>
+		</form>
+	</li>-->
+
 <?php
 	} else {
 		$id = null;
@@ -38,10 +53,22 @@
 	#   TODO: new post should only be diplayed for admin users
 ?>
 
-	<li class="navPoint"><form method="post" aria-label="Make a new post" action="post?newpost"><button class="navLink" type="submit"><i class="navIcon fas fa-edit"></i><p class="navText">&nbsp;New Post</p></button></form></li>
+	<!--<li class="navPoint"><form method="post" aria-label="Make a new post" action="post?newpost"><button class="navLink" type="submit"><i class="navIcon fas fa-edit"></i><p class="navText">&nbsp;New Post</p></button></form></li>-->
 
-    <li class="navPoint right"><form method="post" aria-label="Your account" action="user?userid='<?=$user->id?>'"><button class="navLink" type="submit"><i class="navIcon fas fa-user"></i><p class="navText">&nbsp;<?=$user->name?></p></button></form></li>
-    <li class="navPoint right"><button class="navLink" onclick="openForm('logout')"><i class="navIcon fas fa-sign-out-alt"></i><p class="navText">&nbsp;Logout</p></button></li>
+    <li class="navPoint right">
+    	<form method="post" aria-label="Your account" action="user?userid='<?=$user->id?>'">
+    		<button class="navLink" type="submit">
+    			<i class="navIcon fas fa-user"></i>
+    			<p class="navText">&nbsp;<?=$user->name?></p>
+    		</button>
+    	</form>
+    </li>
+    <li class="navPoint right">
+    	<button class="navLink" onclick="openForm('logout')">
+    		<i class="navIcon fas fa-sign-out-alt"></i>
+    		<p class="navText">&nbsp;Logout</p>
+    	</button>
+    </li>
 	
 <?php
 	}
