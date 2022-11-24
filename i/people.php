a80a5638485e59ec1f66a7c91a4e8db2fe937228
Benedikt Galbavy <if22b082@technikum-wien.at>
2022-11-24 18:37:30 +0100

    portrait

--- i/people.php
+++ i/people.php
@@ -1,6 +1,6 @@
-<div class="grid equal">
+<div class="grid gridpost equal">
 	<div class="post main gridleft">
-		<img src="bega.jpg">
+		<img class="portrait" src="bega.jpg">
 		<h3>Beni</h3>
 		<h4>Benedikt Galbavy</h4>
 		<br/>
@@ -9,7 +9,7 @@
 		<p>E-Mail: <a href="mailto:if22b082@technikum-wien.at">if22b082@technikum-wien.at</a></p>
 	</div>
 	<div class="post main gridright">
-		<img src="jaos.jpg">
+		<img class="portrait" src="jaos.jpg">
 		<h3>Jess</h3>
 		<h4>Jasmin Ossman</h4>
 		<br/>
