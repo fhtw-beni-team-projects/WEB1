e207b8717cb120b1481f3952fc9ace2110c2606e
ossma <oss19129@spengergasse.at>
2023-01-15 23:03:11 +0100

    fileLoader fixes 2

--- i/fileLoader/upload.php
+++ i/fileLoader/upload.php
@@ -1,6 +1,5 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]); # TODO: news name pattern
+$target_file = "../../img/".$_POST["newsId"]."_news.jpg";
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
@@ -8,7 +7,6 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
   } else {
     echo "File is not an image.";
@@ -42,8 +40,6 @@ if ($uploadOk == 0) {
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    $imgFolder = "../img/"+ $_POST["newsId"] + "_news.jpg";
-    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $imgFolder);
 
   } else {
     echo "Sorry, there was an error uploading your file.";
