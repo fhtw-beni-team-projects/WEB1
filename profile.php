7d4dc5e50fd998204c9b3f083e9dd328af946628
Benedikt Galbavy <if22b082@technikum-wien.at>
2023-01-15 18:03:11 +0100

    password changing part 1

--- profile.php
+++ profile.php
@@ -14,15 +14,23 @@
                 ?>
                 <div class="grid maingrid">
                     <div class="feed">
-                        <div class="post main">
-                            <form method="post" action="?submit" class="form formmax">
-                                <div class="formcontent formcontentmax">
+                        
+
+                        <form class="post main form formmax" method="post" action="?submit">
+                            <div class="formcontent formcontentmax">
                                 <p class="descr formleft">Username</p><input class="formright forminput" type="text" name="username" placeholder="Choose a username" value="<?=$user->user['username']?>" />
                                 <p class="descr formleft">E-mail</p><input class="formright forminput" type="text" name="username" placeholder="Choose a email" value="<?=$user->user['email']?>" />
                                 <p class="descr formleft">Firstname</p><input class="formright forminput" type="text" name="fname" placeholder="Choose a firstname" value="<?=$user->user['fname']?>" />
                                 <p class="descr formleft">Lastname</p><input class="formright forminput" type="text" name="lname" placeholder="Choose a lastname" value="<?=$user->user['lname']?>" />
-                                <p class="descr formleft">Password</p><input class="formright forminput" type="text" name="lname" placeholder="Choose a lastname" value="<?=$user->user['pwd_hash']?>" />
-                            </form>
+                            </div>
+                        </form>
+
+                        <div class="post main">
+                            <div class="formcontent grid">
+                                <label class="descr formleft" for="pwd">Altes&nbsp;Passwort</label><input class="popup_input formright forminput" type="password" name="pwd" placeholder="Gib dein altes Passwort ein" />
+                                <label class="descr formleft" for="new_pwd">Neues&nbsp;Passwort</label><input class="popup_input formright forminput" type="password" name="new_pwd" placeholder="Gib ein neues Passwort ein" />
+                                <label class="descr formleft" for="new_pwd2">Wiederhole&nbsp;Passwort</label><input class="popup_input formright forminput" type="password" name="new_pwd2" placeholder="Wiederhole dein neues Passwort" />
+                            </div>
                         </div>
                     </div>
                 </div>
